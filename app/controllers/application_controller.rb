ec2749879c9e59571c649ea8efe4a89b780aca42
Jonathan Tuzman <tuzmusic@gmail.com>
2018-12-10 11:26:46 -0500

    add sessions config

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < Sinatra::Base
     
   register Sinatra::ActiveRecordExtension
-  set :session_secret, "my_application_secret"
-  set :views, Proc.new { File.join(root, "../views/") }
   
+  configure do
+    enable :sessions
+    set :session_secret, "sugar_secret"
+    set :public_folder, 'public'
+    set :views, 'app/views'
+  end
+
+
   get '/' do
     erb :index
   end
