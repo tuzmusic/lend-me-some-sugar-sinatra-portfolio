17e67d85dda53419cb284d7067b719756ca69405
Jonathan Tuzman <tuzmusic@gmail.com>
2018-12-12 13:49:36 -0500

    a few changes to specs before merging

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'pry'
 require_relative '../spec_helper'
 describe 'User' do
   before do
-    @user = User.create(:name => "test 123", :email => "test123@aol.com", :password => "test")
+    @user = User.create(:username => "test 123", :email => "test123@aol.com", :password => "test")
   end
   
   it 'can slug the username' do
@@ -11,7 +11,7 @@ describe 'User' do
 
   it 'can find a user based on the slug' do
     slug = @user.slug
-    expect(User.find_by_slug(slug).name).to eq("test 123")
+    expect(User.find_by_slug(slug).username).to eq("test 123")
   end
 
   it 'has a secure password' do
--- spec/views/user_views_spec.rb
+++ spec/views/user_views_spec.rb
@@ -3,7 +3,7 @@ require 'pry'
 
 describe 'Views' do
   describe "Layout" do
-    it "includes a header" do
+    xit "includes a header" do
       get '/index'
       expect(page.body).to include("Lend Me Some Sugar")
       expect(page.body).to include("Users")
@@ -14,13 +14,13 @@ describe 'Views' do
 
   describe "home page" do
     it "includes a link to sign up" do
-      get '/'
-      expect(page.body).to include("Sign Up")
+      visit '/'
+      expect(page.body).to include("sign up")
     end
     
     it "includes a form to log in" do
-      get '/'
-      expect(page).to have_field(:name)
+      visit '/'
+      expect(page).to have_field(:username)
       expect(page).to have_field(:password)
     end
     
