2cdea683d5cb1cfdd5edd4f15ff2b669bde37593
Jonathan Tuzman <tuzmusic@gmail.com>
2018-12-12 11:56:36 -0500

    ingredient header on show page

--- spec/controllers/show_ingredient_spec.rb
+++ spec/controllers/show_ingredient_spec.rb
@@ -8,7 +8,7 @@ describe "show ingredient action" do
       it "does not let a user view an ingredient show page if logged out" do
         user = User.create(username: "becky567", email: "starz@aol.com", password: "kittens")
         ingredient = Ingredient.create(name: 'parsley', user_id: user.id)
-        
+
         get "/ingredients/#{ingredient.id}"
 
         expect(last_response.status).to eq(302)
@@ -23,12 +23,12 @@ describe "show ingredient action" do
         user = User.create(username: "becky567", email: "starz@aol.com", password: "kittens")
         post '/login', { username: "becky567", password: "kittens" }
 
-        parsley = Ingredient.create(name: 'parsley', user_id: user.id)
+        Ingredient.create(name: 'parsley', user_id: user.id)
       end
       
-      it "lets the user access the ingredient show page if logged in" do
-        get "/ingredients/#{ingredient.id}"
-        expect(last_response.body).to include(ingredient.name)
+      it "shows the name of the ingredient" do
+        get "/ingredients/#{Ingredient.first.id}"
+        expect(last_response.body).to include('parsley')
       end
 
       it "lists all the users who have this ingredient" do
